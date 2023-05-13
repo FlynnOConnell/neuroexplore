de92d9ea1316722e2a88ff1d987880ac91cf7e9b
NeuroPyPy <Flynnoconnell@gmail.com>
2023-05-13 13:27:20 -0400

    separate eating/stimuli signals classes

--- data_utils/data_collection.py
+++ data_utils/data_collection.py
@@ -9,8 +9,34 @@ from . import signals_food as sf
 from . import signals_stimuli as ss
 from params import Params
 
+def concat_dataframe_to_dataframe(df1, df2):
+    # Find the columns that are in df2 but not in self.means_df
+    new_columns = df2.columns.difference(df1.columns)
+
+    # Add the new columns to self.means_df with NaN values
+    for col in new_columns:
+        df1[col] = np.nan
+
+    # Find the columns that are in self.means_df but not in df2
+    missing_columns = df1.columns.difference(df2.columns)
+
+    # Add the missing columns to df2 with NaN values
+    for col in missing_columns:
+        df2[col] = np.nan
+
+    # Concatenate df2 to self.means_df along the rows (axis=0)
+    updated_df = pd.concat([df1, df2], axis=0)
+
+    # Reorder the columns based on the original self.means_df
+    updated_df = updated_df[df1.columns]
+
+    # Reset the index
+    updated_df.reset_index(drop=True, inplace=True)
+
+    return updated_df
+
 class DataCollection:
-    def __init__(self, directory: str | Path = '', filenames = None):
+    def __init__(self, directory: str | Path = ''):
         self.params = Params()
         self.stats = None
         self.info_df = pd.DataFrame(columns=['animal', 'date', '#timestamps'])
@@ -25,6 +51,10 @@ class DataCollection:
         self.errors = {}
 
     def get_data(self, paradigm='sf', num_files=None, functions_to_run=None,):
+        """
+        Instantiate Signals class for given paradigm. If num_files is not None, only the first num_files will be used.
+        Sorted by paradigm, then filename.
+        """
         if paradigm not in ['sf', 'rs']:
             raise ValueError(f'Paradigm {paradigm} not found: Options are sf or rs.')
         if num_files:
@@ -47,7 +77,11 @@ class DataCollection:
                 tb_lines = ''.join(tb_str)
                 self.errors[file] = f"{e}\n{tb_lines}"
 
-    def get_data_file(self, filenames, paradigm='sf', functions_to_run=None,):
+    def get_data_by_filename(self, filenames, paradigm='sf', functions_to_run=None,):
+        """
+        Instantiate Signals class for given filename(s).
+        filenames can be a single string or an iterable of strings.
+        """
         nexdata = {}
         if paradigm not in self.data_files:
             raise ValueError(f'Paradigm {paradigm} not found.')
@@ -68,55 +102,15 @@ class DataCollection:
                     self.errors[file] = f"{e}\n{tb_lines}"
         return nexdata
 
-    @staticmethod
-    def concat_dataframe_to_dataframe(df1, df2):
-        # Find the columns that are in df2 but not in self.means_df
-        new_columns = df2.columns.difference(df1.columns)
-
-        # Add the new columns to self.means_df with NaN values
-        for col in new_columns:
-            df1[col] = np.nan
-
-        # Find the columns that are in self.means_df but not in df2
-        missing_columns = df1.columns.difference(df2.columns)
-
-        # Add the missing columns to df2 with NaN values
-        for col in missing_columns:
-            df2[col] = np.nan
-
-        # Concatenate df2 to self.means_df along the rows (axis=0)
-        updated_df = pd.concat([df1, df2], axis=0)
-
-        # Reorder the columns based on the original self.means_df
-        updated_df = updated_df[df1.columns]
-
-        # Reset the index
-        updated_df.reset_index(drop=True, inplace=True)
-
-        return updated_df
-
-    def get_stats(self, paradigm='sf'):
-        for file in self.files[paradigm]:
+    def get_eating_stats(self):
+        for file in self.files['rf']:
             for filename, data in file.items():
                 session_info = parse_filename(filename)
                 session = [session_info.animal, session_info.date]
                 for neuron in data.neurons:
                     self.info_df.loc[len(self.info_df)] = session + [data.num_ts[neuron]]
-                self.means_df = self.concat_dataframe_to_dataframe(self.means_df, data.means)
-                self.sems_df = self.concat_dataframe_to_dataframe(self.sems_df, data.sems)
-                if self.means_df.shape != self.sems_df.shape:
-                    raise ValueError('Means and SEMs dataframes are not the same size.')
-        self.stats = pd.concat([self.info_df, self.means_df, self.sems_df], axis=1)
-
-    def get_rs(self, paradigm='rs'):
-        for file in self.files[paradigm]:
-            for filename, data in file.items():
-                session_info = parse_filename(filename)
-                session = [session_info.animal, session_info.date]
-                for neuron in data.neurons:
-                    self.info_df.loc[len(self.info_df)] = session + [data.num_ts[neuron]]
-                self.means_df = self.concat_dataframe_to_dataframe(self.means_df, data.means)
-                self.sems_df = self.concat_dataframe_to_dataframe(self.sems_df, data.sems)
+                self.means_df = concat_dataframe_to_dataframe(self.means_df, data.means)
+                self.sems_df = concat_dataframe_to_dataframe(self.sems_df, data.sems)
                 if self.means_df.shape != self.sems_df.shape:
                     raise ValueError('Means and SEMs dataframes are not the same size.')
         self.stats = pd.concat([self.info_df, self.means_df, self.sems_df], axis=1)
